a48d14e82725e167b63499a4987b05e01b1eba9f
bhagi221206 <bhagi221206@gmail.com>
2025-12-09 21:59:12 +0530

    Add files via upload

---
+++ Multiplication.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    long long num1, num2;
+    scanf("%11d", &num1);
+    scanf("%11d", &num2);
+    long long result = num1 * num2;
+    printf("%11d", result);
+    return 0;
+}
