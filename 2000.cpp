b5814f1b1141ad02c774a79ca6239773ea32ea0e
bhagi221206 <bhagi221206@gmail.com>
2025-12-09 22:00:00 +0530

    Add files via upload

---
+++ 2000.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    
+    // Read the number of ?2000 notes
+    scanf("%d", &N);
+    
+    // Calculate the number of ?500 notes needed
+    int result = N * 4;
+    
+    // Output the result
+    printf("%d
+", result);
+    
+    return 0;
+}
