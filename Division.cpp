c63b367a01a7acd52e4263961a435ae213989fad
bhagi221206 <bhagi221206@gmail.com>
2025-12-09 21:57:30 +0530

    Add files via upload

---
+++ Division.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int num1, num2;
+    float result;
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+    result = (float) num1 / num2;
+    printf("%d", num1 / num2);
+    return 0;
+}
