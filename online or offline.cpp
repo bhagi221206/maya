5c912e55e147c80f67810760e3becc08a66b0d75
bhagi221206 <bhagi221206@gmail.com>
2025-12-09 20:48:01 +0530

    Add files via upload

---
+++ online or offline.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int o, r;
+    scanf("%d %d", &o, &r);
+    if (o - o * 0.1 > r)
+    
+       { printf("DINING");}
+       else if (o - o * 0.1 == r)
+       {printf("EITHER");}
+       else
+       {printf("ONLINE");}
+       return 0;
+}
+       
+
