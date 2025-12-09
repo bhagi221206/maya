60a27ec62e42f609188163aa8afe4b3e24730077
bhagi221206 <bhagi221206@gmail.com>
2025-12-09 16:09:36 +0530

    Add files via upload

---
+++ the gift.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int x, n, m;
+    scanf("%d %d %d",&x, &n, &m);
+    if ((n-x) <= m)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    return 0;
+}
+
