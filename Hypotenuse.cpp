a78078fa2adfc06849ab47706a583fc986db572c
bhagi221206 <bhagi221206@gmail.com>
2025-12-10 08:35:51 +0530

    Add files via upload

---
+++ Hypotenuse.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int x, y;
+    double hypotenuse;
+
+    // Read input
+    scanf("%d %d", &x, &y);
+
+    // Calculate hypotenuse
+    hypotenuse = sqrt(x * x + y * y);
+
+    // Print result with 2 decimal places
+    printf("%.2f
+", hypotenuse);
+
+    return 0;
+}
